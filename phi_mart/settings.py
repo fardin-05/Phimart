6823348fd36beeac3c3cebec6e2abf3535e64c66
Fardin Ahmed Azim <fardinazim7@gmail.com.com>
2025-11-14 00:23:32 +0600

    commited

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -74,16 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'phi_mart.wsgi.app'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -205,3 +195,9 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+
+
+if not DEBUG:
+    INSTALLED_APPS.remove('debug_toolbar')
+    MIDDLEWARE.remove("debug_toolbar.middleware.DebugToolbarMiddleware")
